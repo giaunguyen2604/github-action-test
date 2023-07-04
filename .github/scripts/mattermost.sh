ec761b0a449aa8b6ac89e9d8cd738549292bb08c
giaunguyen2604 <giau.nguyen@executionlab.asia>
2023-07-04 16:05:46 +0700

    update

--- .github/scripts/mattermost.sh
+++ .github/scripts/mattermost.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ENV=$1
 VERSION=$(bash .github/scripts/version.sh)
 
-echo "{\"text\":\"![](https://img.shields.io/badge/dev-blue?label=environment)  ![](https://img.shields.io/badge/${VERSION}-red?label=version)\\n\\nRelease Frontend done! :agon-clap: \\nURL: https://order.dev.banhmipate.com\\n\\n#### :noted: Release notes: \\n---\\n:bulb: **New features/Enhancements:**\\n${FEAT_NOTES}\\n---\\n:ladybug: **Bug fixes:**\\n${BUG_NOTES}\\n\\ncc @trang.nguyen @toan.le @vy.huynh @giau.nguyen @hung.huynh @thu.bui @danh.nguyen\\\"}\"}" > mattermost.json
+echo "{\"text\":\"![](https://img.shields.io/badge/${ENV}-blue?label=environment)  ![](https://img.shields.io/badge/1.1.0-red?label=version)\\n\\nRelease **Frontend** done! :agon-clap: \\nURL: https://order.dev.banhmipate.com\\n\\n#### :noted: Release notes: \\n---\\n:bulb: **New features/Enhancements:**\\n${FEAT_NOTES}---\\n:ladybug: **Bug fixes:**\\n${BUG_NOTES}\\ncc @trang.nguyen @toan.le @vy.huynh @giau.nguyen @hung.huynh @thu.bui @danh.nguyen\\\"}\"}" > mattermost.json
