2684653369d0e94a242b7edfd43fd128eb7668d5
giaunguyen2604 <giau.nguyen@executionlab.asia>
2023-07-04 13:45:41 +0700

    update

---
+++ scripts/mattermost.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+VERSION=$(bash ./version.sh)
+
+echo "{\"text\":\"![](https://img.shields.io/badge/dev-blue?label=environment)  ![](https://img.shields.io/badge/${VERSION}-red?label=version)\\n\\nRelease `Frontend` done! :agon-clap: \\nURL: https://order.dev.banhmipate.com\\n\\n#### :noted: Release notes: \\n---\\n:bulb: **New features/Enhancements:**\\n${FEAT_NOTES}\\n---\\n:ladybug: **Bug fixes:**\\n${BUG_NOTES}\\ncc @trang.nguyen @toan.le @vy.huynh @giau.nguyen @hung.huynh @thu.bui @danh.nguyen\\\"}\"}" > mattermost.json
